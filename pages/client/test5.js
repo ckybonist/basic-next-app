ce388f3238ff0364de70d1aaf33bdf77a0dfd4b2
ckybonist <ckybonist@gmail.com>
2020-11-14 14:37:51 +0800

    [Debug] log something on DOM

--- pages/client/test5.js
+++ pages/client/test5.js
@@ -1,14 +1,41 @@
-const Test5 = () => (
+const isClient = typeof window !== 'undefined';
+
+const Test5 = () => isClient ? (
   <>
-    <h1>Client Test 5</h1>
-    <button
-      onClick={() => {
-        throw new Error('Client Test 5')
-      }}
-    >
-      Click me to throw an Error
-    </button>
+    <h1 id="target-elem">Client Test 5</h1>
+    <p>navigator.standalone: <br /> <code>{String(navigator.standalone)}</code></p>
+    <p>createEvent: <code> <br /> {String(document.createEvent)}</code></p>
+    <p>target-elem recieve event with type: <code id="target-code"></code></p>
+    <div>
+      <button onClick={() => {
+        const event = document.createEvent('Event');
+        event.initEvent('foo', true, true)
+        const elem = document.getElementById('target-elem');
+        if (elem) {
+          elem.addEventListener('foo', (event) => {
+            console.log(event);
+            const outputElem = document.getElementById('target-code');
+            if (outputElem) {
+              outputElem.innerText = event.type;
+            }
+          })
+          elem.dispatchEvent(event);
+        }
+      }}>
+        Click me to create event
+      </button>
+    </div>
+    <br />
+    <div>
+      <button
+        onClick={() => {
+          throw new Error('Client Test 5')
+        }}
+      >
+        Click me to throw an Error
+      </button>
+    </div>
   </>
-)
+) : null;
 
 export default Test5
